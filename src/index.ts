e9569c34425edbc1e2f0eadda54aeb406d3673e3
nick <nick@prnd.co.kr>
2019-09-21 13:25:18 +0900

    circle에 색상, 사이즈를 랜덤으로 적용하고 mouseover 인터랙션을 추가한다

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,60 @@
 const canvas = document.createElement('canvas');
 const wrapper = document.querySelector('#canvasWrapper');
 
-if(wrapper) {
-  wrapper.appendChild(canvas);
-}
-
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
-const c = canvas.getContext('2d')!;
+if (wrapper) {
+  wrapper.appendChild(canvas);
+}
 
+const mouse: { x: number | undefined, y: number | undefined } = {
+  x: undefined,
+  y: undefined
+};
+
+window.addEventListener('mousemove', (event) => {
+  mouse.x = event.x;
+  mouse.y = event.y;
+});
+
+window.addEventListener('resize', () => {
+  canvas.width = innerWidth;
+  canvas.height = innerHeight;
+  init()
+});
+const c = canvas.getContext('2d')!;
+const MAX_RADIUS = 40;
+// const MIN_RADIUS = 2;
+
+const colorArray = [
+  '#67493C',
+  '#E68F71',
+  '#FDCA95',
+  '#FEE2C3',
+  '#C0CBB7'
+];
+
+let circleArray: Array<Circle> = [];
+const init = () => {
+  circleArray = [];
+  for (let i = 0; i < 800; i++) {
+    const radius = Math.random() * 3 + 1;
+    const x = Math.random() * (innerWidth - radius * 2) + radius;
+    const y = Math.random() * (innerHeight - radius * 2) + radius;
+    const dx = Math.random() - 0.5;
+    const dy = Math.random() - 0.5;
+    circleArray.push(new Circle(x, y, dx, dy, radius));
+  }
+}
 class Circle {
   private x: number;
   private y: number;
   private dx: number;
   private dy: number;
-  private readonly radius: number;
+  private radius: number;
+  private minRadius: number;
+  private color: string;
 
   constructor(x: number, y: number, dx: number, dy: number, radius: number) {
     this.x = x;
@@ -23,13 +62,15 @@ class Circle {
     this.dx = dx;
     this.dy = dy;
     this.radius = radius;
+    this.minRadius = radius;
+    this.color = colorArray[Math.floor(Math.random() * colorArray.length)];
+
   }
 
   draw() {
     c.beginPath();
     c.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-    c.strokeStyle = 'blue';
-    c.stroke();
+    c.fillStyle = this.color;
     c.fill();
   }
 
@@ -43,20 +84,21 @@ class Circle {
     }
     this.x = this.x + this.dx;
     this.y = this.y + this.dy;
+
+    // interactivity
+    if (mouse.x && mouse.y) {
+      if (mouse.x - this.x < 50 && mouse.x - this.x > -50 && mouse.y - this.y < 50 && mouse.y - this.y > -50) {
+        if (this.radius < MAX_RADIUS)
+          this.radius += 1
+      } else if (this.radius > this.minRadius) {
+        this.radius -= 1
+      }
+    }
   }
+
+
 }
 
-
-
-const circleArray: Array<Circle> = [];
-for(let i = 0; i<100; i++ ){
-  const radius = 30;
-  const x = Math.random() * (innerWidth - radius * 2) + radius;
-  const y = Math.random() * (innerHeight - radius * 2) + radius;
-  const dx = Math.random() - 0.5;
-  const dy = Math.random() - 0.5;
-  circleArray.push(new Circle(x, y, dx, dy, radius));
-}
 
 const animate = () => {
   requestAnimationFrame(animate);
@@ -66,7 +108,7 @@ const animate = () => {
     circle.draw();
     circle.update();
   })
-
 };
 
+init();
 animate();
